5d2d2918ab89ca18ca4f20ffe362c5d4854b10ea
doctor82 <80136584+doctor82@users.noreply.github.com>
2022-08-20 11:19:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 //https://teachablemachine.withgoogle.com/models/S6PXWgiSP/
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/S6PXWgiSP/model.json', modelReady);
+    }
+    
+    function modelReady(){
+    classifier.classify(gotResults);
+    
+    }
+    function gotResults(error, results){
+    
+    }
