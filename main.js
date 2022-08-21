58ed004d569c9e9e291b4a75097c53a1113501f1
doctor82 <80136584+doctor82@users.noreply.github.com>
2022-08-21 13:14:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,5 +9,35 @@ function startClassification(){
     
     }
     function gotResults(error, results){
+        if (error) {
+            console.error(error);
+        
+            }else{
+          console.log(results);
+          random_number_r=Math.floor(Math.random()*255)+1;
+          random_number_g=Math.floor(Math.random()*255)+1;
+          random_number_b=Math.floor(Math.random()*255)+1;
+        
+        document.getElementById("result_label").innerHTML="Detected Voice- "+results[0].label;
+        document.getElementById("result_confidence").innerHTML="Accuracy- "+(results[0].confidence*100).toFixed(3)+"%";
+        document.getElementById("result_label").style.color="rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+        document.getElementById("result_confidence").style.color="rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+     
+        
+        img=document.getElementById('Image');
+        if (results[0].label=="Barking") {
+          img.src="Dog.jpg";
+      
+          }else if(results[0].label=="Meowing"){
+            img.src="download.jpg";
+      
+          }
+          else{
+  
+            img.src="Cow.jpg"; 
+              }
+       
+            }}
+
     
-    }
+ 
